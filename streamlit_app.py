57fa10aeb0cdde8786c6ad743c3145421a72f590
bibhuti037 <39851321+bibhuti037@users.noreply.github.com>
2023-07-19 15:21:47 +0530

    Update streamlit_app.py
    Added table fetch

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,3 +40,10 @@ my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
+
+
+my_cur=my_cnx.cursor()
+my_cur.execute("Select * From fruit_load_list")
+my_data_rows=my_cur.fetchall()
+streamlit.header('The Fruit Load List Contains:')
+streamlit.dataframe(fruits_to_show)
